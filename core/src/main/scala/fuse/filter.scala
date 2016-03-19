d6e2ee802260cee5f33cce7a3e8ecd134d546ae4
Paul Phillips <paulp@improving.org>
2016-03-19 08:50:45 -0700

    Extract the Id part of idfs.
    Move implementation into common superclass. Create handful
    of simple filesystems to begin exercising various possibilities.

--- core/src/main/scala/fuse/filter.scala
+++ core/src/main/scala/fuse/filter.scala
@@ -3,21 +3,12 @@ package fuse
 
 import jio._
 
-/** Forwarding filesystem which only passes through paths which match the filter.
- */
-class FilteredFs(protected val underlying: FuseFilesystem, cond: String => Boolean) extends ForwarderFs {
-  override def readdir(path: String, df: DirectoryFiller): Int =
-    underlying.readdir(path, new FilteredDirFiller(df, cond))
-}
 
 class FilteredDirFiller(filler: DirectoryFiller, condition: String => Boolean) extends DirectoryFiller {
   def add(files: jIterable[String]): Boolean = filler add (files.asScala filter condition).asJava
   def add(files: String*): Boolean           = filler add (files filter condition).asJava
 }
-
-object filterfs {
-  def main(args: Array[String]): Unit = args.toList match {
-    case from :: to :: regex :: Nil => idfs(path(from)) filterNot (_ matches regex) mountForeground path(to)
-    case _                          => println("Usage: filter <from> <to> <regex>")
-  }
+class MappedDirFiller(filler: DirectoryFiller, f: String => String) extends DirectoryFiller {
+  def add(files: jIterable[String]): Boolean = filler add (files.asScala map f).asJava
+  def add(files: String*): Boolean           = filler add (files map f).asJava
 }
---
+++ core/src/main/scala/fuse/runner.scala
@@ -0,0 +1,47 @@
+package sfs
+package fuse
+
+import jio._
+
+object idfs extends FsRunner {
+  def runMain = { case Array(from, to) =>
+    start(new Rooted(from) logging, to)
+  }
+}
+object filterfs extends FsRunner {
+  override def usage = "<from> <to> <regex>"
+  def runMain = { case Array(from, to, regex) =>
+    start(new Rooted(from) filterNot (_ matches regex), to)
+  }
+}
+object reversefs extends FsRunner {
+  trait Reverser extends RootedFs {
+    override protected def pathBytes(path: Path): Array[Byte] =
+      super.pathBytes(path).reverse
+  }
+  def runMain = { case Array(from, to) =>
+    start(new Rooted(from) with Reverser, to)
+  }
+}
+
+/** Reverses all the data on the filesystem.
+ */
+/** Generic SFS runner.
+ */
+abstract class FsRunner {
+  def runMain: Array[String] =?> Unit
+
+  def name: String                                = this.getClass.shortName
+  def usage: String                               = "<from> <to>"
+  def start(fs: FuseFs, mountPoint: String): Unit = fs mountForeground path(mountPoint)
+
+  class Rooted(val root: Path) extends RootedFs {
+    def this(root: String) = this(path(root))
+    def getName = name
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (runMain isDefinedAt args) runMain(args)
+    else Console.err.println(s"Usage: $name $usage")
+  }
+}
