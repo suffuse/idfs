c3bfba8f618daaee0109e1e173c170a7fd6ca015
Dale Wijnand <dale.wijnand@gmail.com>
2017-02-19 23:47:17 -0500

    Fix & complement runner 1-line docs

--- core/src/main/scala/fuse/runner.scala
+++ core/src/main/scala/fuse/runner.scala
@@ -3,17 +3,23 @@ package fuse
 
 import jio._, api._
 
+/** The identity filesystem.
+  */
 object idfs extends FsRunner {
   def runMain = { case Array(from, to) =>
     start(new Rooted(from).logging, to)
   }
 }
+/** Only passes through paths which match the given regex.
+ */
 object filterfs extends FsRunner {
   override def usage = "<from> <to> <regex>"
   def runMain = { case Array(from, to, regex) =>
     start(new Rooted(from) filterNot (_ matches regex), to)
   }
 }
+/** Reverses all the data on the filesystem.
+ */
 object reversefs extends FsRunner {
   trait Reverser extends RootedFs {
     override protected def pathBytes(path: Path): Array[Byte] =
@@ -24,8 +30,6 @@ object reversefs extends FsRunner {
   }
 }
 
-/** Reverses all the data on the filesystem.
- */
 /** Generic SFS runner.
  */
 abstract class FsRunner {
